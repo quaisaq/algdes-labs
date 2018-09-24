fe60004ec394832c625d950d31310d0c1b7ad2ba
marcus joost <trolljoost@gmail.com>
2018-09-24 10:52:03 +0200

    algo implement

--- gorilla/src/Main.java
+++ gorilla/src/Main.java
@@ -8,13 +8,21 @@ public class Main {
     private static int[][] memoizer;
     private static int N;
     private static int[][] costs;
+    private static int d;
 
     public static void main(String[] args) {
         assert args.length == 2;
         parseCost(args[0]);
         parseInput(args[1]);
-        Result result = alg();
-        printOutput(result);
+        for(int i = 0; i < sequences.length; i++){
+            for(int j = i+1; sequences.length; j++){
+                char[] s1 = sequences[i];
+                char[] s2 = sequences[j];
+                Result result = alg(s1, s2);
+                printOutput(result);
+            }
+        }
+        
     }
 
     public static void parseInput(String file) {
@@ -27,8 +35,27 @@ public class Main {
         throw new RuntimeException("Not implemented");
     }
 
-    public static Result alg() {
-        throw new RuntimeException("Not implemented");
+    public static Result alg(char[] s1, char[] s2) {
+        int m = s1.length;
+        int n = s2.length;
+        memoizer = new int[m+1][n+1];
+
+        for(int i = 0; i < m; i++){
+            memoizer[i][0] = d;
+        }
+        for(int j = 0; j < n; j++){
+            memoizer[0][j] = d;
+        }
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                int val1 = cost[s1[i]][s2[j]] + memoizer[i-1][j-1];
+                int val2 = d + memoizer[i-1][j];
+                int val3 = d + memoizer[i][j-1]; 
+                memoizer[i][j] = Math.min(val1, Math.min(val2, val3));
+            }
+        }
+        return memoizer[m][n];
     }
 
     public static void printOutput(Result result) {
