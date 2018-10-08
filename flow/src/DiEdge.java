d517745c4cce47f93c832164baac08008d21d69a
Rune Kofoed-Nissen <rune.kofoed-nissen@agillic.com>
2018-10-08 11:18:15 +0200

    Stuff

--- flow/src/DiEdge.java
+++ flow/src/DiEdge.java
@@ -1,5 +1,13 @@
 public class DiEdge <T> {
     private T value;
+    private Vertex to;
+    private Vertex from;
+
+    public DiEdge(Vertex from, Vertex to) {
+        this.to = to;
+        this.from = from;
+        from.addEdge(this);
+    } 
 
     public T getValue() {
         return value;
@@ -7,5 +15,13 @@ public class DiEdge <T> {
 
     public void setValue(T value) {
         this.value = value;
+    }
+
+    public Vertex getTo() {
+        return to;
+    }
+
+    public Vertex getFrom() {
+        return from;
     }
 }
--- flow/src/RestDiEdge.java
+++ flow/src/RestDiEdge.java
@@ -1,9 +1,22 @@
 public class RestDiEdge extends DiEdge<Integer> {
+    private RestDiEdge counterpart;
+
+    public RestDiEdge(Vertex from, Vertex to) {
+        super(from, to);
+        this.counterpart = new RestDiEdge(to, from, this);
+        to.addEdge(counterpart);
+    }
+
+    private RestDiEdge(Vertex from, Vertex to, RestDiEdge counterpart) {
+        super(from, to);
+        this.counterpart = counterpart;
+    }
+
     public void setRestValue(int x) {
         // Set the value
-        int y = getValue() - x;
+        int difference = getValue() - x;
         setValue(x);
         // Update the edge counterpart
-        counterpart.setValue(y + counterpart.getValue());
+        counterpart.setValue(difference + counterpart.getValue());
     }
 }
