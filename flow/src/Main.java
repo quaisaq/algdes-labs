4aa18fe86d843879d36a94be67ab3dc7b185ef8e
Rune Kofoed-Nissen <rune.kofoed-nissen@agillic.com>
2018-10-22 12:05:57 +0200

    Fixed readinput

--- flow/src/Main.java
+++ flow/src/Main.java
@@ -26,47 +26,35 @@ public class Main {
             FlowGraph graph = new FlowGraph();
 
             ArrayList<Vertex> vertices = new ArrayList<>();
-            int vertexCount = 0;
-            int edgeCount = 0;
-            boolean parsingVertices = true;
 
             assert sc.hasNext();
-            vertexCount = Integer.parseInt(sc.nextLine());
-
-            int currentVertexIndex = 0;   // Used to assign index to vertices
-            
-            while (sc.hasNext()) {
+            int vertexCount = Integer.parseInt(sc.nextLine());
+            for(int currentVertexIndex = 0; currentVertexIndex < vertexCount; currentVertexIndex++) {
+                assert sc.hasNext();
                 String line = sc.nextLine();
-                
-                if (line.equals("DESTINATIONS")) {
-                    if (sc.hasNext()) {
-                        edgeCount = Integer.parseInt(sc.nextLine());
-                        // System.out.println("Edge count: " + edgeCount);
-                    }
-                    parsingVertices = false;
-                    continue;
-                } else {
-                    if (parsingVertices) {
-                        Vertex v = new Vertex(currentVertexIndex++, line);
-                        vertices.add(v);
-                    } else {
-                        String[] components = line.split(" ");                          // v1 v2 capacity
+                Vertex v = new Vertex(currentVertexIndex, line);
+                vertices.add(v);
+            }
+            assert sc.hasNext();
+            int edgeCount = Integer.parseInt(sc.nextLine());
+            for(int currentEdgeIndex = 0; currentEdgeIndex < edgeCount; currentEdgeIndex++) {
+                assert sc.hasNext();
+                String line = sc.nextLine();
+                String[] components = line.split(" ");                          // v1 v2 capacity
                         
-                        Vertex v1 = vertices.get(Integer.parseInt(components[0]));
-                        Vertex v2 = vertices.get(Integer.parseInt(components[1]));
-                        int capacity = Integer.parseInt(components[2]);
-                        if(capacity == -1)
-                            capacity = Integer.MAX_VALUE;
+                Vertex v1 = vertices.get(Integer.parseInt(components[0]));
+                Vertex v2 = vertices.get(Integer.parseInt(components[1]));
+                int capacity = Integer.parseInt(components[2]);
+                if(capacity == -1)
+                    capacity = Integer.MAX_VALUE;
 
-                        DiEdge v1_v2 = new RestDiEdge(v1, v2);                              // Create edge from v1 to v2
-                        v1_v2.setValue(capacity);
-                        v1.addEdge(v1_v2);
+                DiEdge v1_v2 = new RestDiEdge(v1, v2);                              // Create edge from v1 to v2
+                v1_v2.setValue(capacity);
+                v1.addEdge(v1_v2);
 
-                        DiEdge v2_v1 = new RestDiEdge(v2, v1);                              // Create edge from v2 to v1
-                        v2_v1.setValue(capacity);
-                        v2.addEdge(v2_v1);
-                    }
-                }
+                DiEdge v2_v1 = new RestDiEdge(v2, v1);                              // Create edge from v2 to v1
+                v2_v1.setValue(capacity);
+                v2.addEdge(v2_v1);
             }
             
             assert vertexCount == vertices.size();
